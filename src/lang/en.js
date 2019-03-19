7b90b901e2674699f8c6e666ae47116981c01f20
sam luo <601438931@qq.com>
2019-03-19 15:50:02 +0800

    初始化

---
+++ src/lang/en.js
@@ -0,0 +1,11 @@
+export default {
+  login: {
+    title: 'Log In',
+    userplaceholder: 'username',
+    passwordplaceholder: 'password',
+    btnlogin: 'sign in'
+  },
+  langswitch: {
+    msg: 'Switch English successfully！'
+  }
+}
---
+++ src/lang/zh.js
@@ -0,0 +1,12 @@
+export default {
+  login: {
+    title: '登录',
+    userplaceholder: '用户名',
+    passwordplaceholder: '密码',
+    btnlogin: '登录'
+  },
+  langswitch: {
+    msg: '切换中文成功！'
+  }
+}
+
